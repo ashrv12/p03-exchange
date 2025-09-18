bd9de2b020fb7ff1ca1c30a57dac974a82e5d5a4
Shrv Bldna <151522619+ashrv12@users.noreply.github.com>
2025-09-19 01:46:32 +0800

    Cleaned it up nicely and made it more intuitive

--- src/lib/components/root/mnt_to_usd.svelte
+++ src/lib/components/root/mnt_to_usd.svelte
@@ -1,26 +1,33 @@
 <script lang="ts">
 	import { emojisToCurrency } from '$lib/emojis_to_currency';
-	import { preferences } from '../../../stores/preferences';
 
 	let { data } = $props();
 	let rates: [string, string][] = Object.entries(data.rates);
 
-	let defaultCurrency = rates.find(([code]) => code === 'USD');
+	// the amount they wish to conver to their selected currency
+	let userAmountInput = $state(1.0);
+	let userConvertedToCurrency = $state('mnt');
 
-	// first block for selecting your currency
-	let first = $state({
-		currency: (rates[0][0] === 'RATE_DATE' ? rates[1][0] : rates[0][0]).toLowerCase(),
-		amount: 1
-	});
+	// usd eur cny type string
+	let selectedTargetCurrency = $state('usd');
+	let selectedTargetCurrencyPrice = $derived(findCurrencyPrice(selectedTargetCurrency));
 
-	// second block for after selecting you show the conversion
-	let second = $state({
-		...$preferences,
-		amount: defaultCurrency ? parseFloat(defaultCurrency[1].replace(',', '') as string) : 696969
-	});
+	// function that takes a three letter currency input and returns the price in float
+	function findCurrencyPrice(selectedCurrency: string) {
+		const defaultCurrency = rates.find(
+			([currencyCode]) => currencyCode === selectedCurrency.toUpperCase()
+		);
+		if (!defaultCurrency) return null;
+
+		return parseFloat(defaultCurrency[1].replace(',', ''));
+	}
 
 	// third block for deriving the converted amount
-	let conversion = $derived(first.amount * second.amount);
+	let conversion = $derived(
+		selectedTargetCurrencyPrice
+			? roundDownTo2Fixed(userAmountInput * selectedTargetCurrencyPrice)
+			: 6969.69
+	);
 
 	// just creating an emoji hash map
 	const emojiMap = Object.fromEntries(
@@ -31,6 +38,10 @@
 	function getEmoji(currencyCode: string) {
 		return emojiMap[currencyCode.toLowerCase()] ?? '🏳️'; // fallback flag
 	}
+
+	function roundDownTo2Fixed(num: number) {
+		return (Math.floor(num * 100) / 100).toFixed(2);
+	}
 </script>
 
 <div class="flex w-full flex-col items-center justify-center gap-y-1 px-2 text-[#edededff]">
@@ -38,22 +49,27 @@
 		<input
 			class="h-[3rem] w-[80%] appearance-none overflow-hidden border-none bg-transparent text-center text-2xl outline-none focus:outline-none"
 			type="number"
-			bind:value={first.amount}
+			bind:value={userAmountInput}
 		/>
-		<div class="flex gap-x-1 text-2xl">
+		<!-- <div class="flex gap-x-1 text-2xl">
 			<h3>{getEmoji(first.currency.toLowerCase())}</h3>
 			<h3>{first.currency}</h3>
-		</div>
+		</div> -->
+		<select bind:value={selectedTargetCurrency}>
+			{#each emojisToCurrency as country}
+				<option value={country.currency}>{country.countryEmoji} {country.currency}</option>
+			{/each}
+		</select>
 	</div>
 	<button class="">
 		<img class="w-[1.5rem]" src="./swap.svg" alt="swap" />
 	</button>
 	<div class="glass flex w-full items-center justify-center rounded-md border-b-[3px] px-3">
-		{#if second.amount === 696969}
+		{#if conversion === 6969.69}
 			<input
 				class="h-[3rem] w-[80%] appearance-none overflow-hidden border-none bg-transparent text-center text-2xl outline-none focus:outline-none"
 				type="text"
-				value="696969"
+				value="6969.69"
 				disabled
 			/>
 		{:else}
@@ -64,9 +80,9 @@
 				disabled
 			/>
 		{/if}
-		<div class="flex gap-x-1 text-2xl">
-			<h3>{getEmoji(second.currency.toLowerCase())}</h3>
-			<h3>{second.currency}</h3>
+		<div class="flex gap-x-1 text-lg">
+			<h3>{getEmoji(userConvertedToCurrency)}</h3>
+			<h3>{userConvertedToCurrency}</h3>
 		</div>
 	</div>
 </div>
