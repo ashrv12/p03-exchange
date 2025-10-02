09b131efd73253bdf6b023dbfaf4895e95744fb4
Shrv Bldna <151522619+ashrv12@users.noreply.github.com>
2025-10-02 21:58:24 +0800

    Asd

--- src/lib/components/root/desktop_content_root.svelte
+++ src/lib/components/root/desktop_content_root.svelte
@@ -1,5 +1,6 @@
 <script>
 	import House from '../svgs/house.svelte';
+	import SearchIcon from '../svgs/search_icon.svelte';
 	import TriangleRight from '../svgs/triangle_right.svelte';
 </script>
 
@@ -17,17 +18,40 @@
 		</div>
 	</div>
 	<div class="z-1 flex w-full grow flex-col rounded-tl-md border-[1px] border-[#404040]/25">
-		<div class="flex h-[10%] w-full items-center border-b-[1px] border-[#404040]/25 px-1">
-			<h1 class="font-rat-thin px-5 text-2xl">Dashboard</h1>
-		</div>
-		<div class="flex w-full grow gap-4 p-5">
+		<div
+			class="flex h-[10%] w-full items-center justify-between border-b-[1px] border-[#404040]/25 px-5"
+		>
+			<h1 class="font-rat-thin text-2xl">Dashboard</h1>
 			<div
-				class="h-[250px] w-[400px] rounded-lg border-[1px] border-[#404040]/25 bg-[#0D0D0D] p-2 hover:translate-x-[-1px] hover:translate-y-[-1px] hover:outline hover:outline-[#1A1A1A]"
+				class="flex items-center gap-x-2 rounded-lg border-[1px] border-[#404040]/25 px-2 py-1 focus-within:outline-1 focus-within:outline-[#a8a8a8]/50"
+			>
+				<SearchIcon />
+				<input
+					class="focus:outline-hidden"
+					name="dash search"
+					type="text"
+					placeholder="search..."
+				/>
+			</div>
+		</div>
+		<div class="flex w-full grow justify-between gap-4 p-5">
+			<div
+				class="h-[250px] w-[400px] rounded-lg border-[1px] border-[#404040]/25 bg-[#0D0D0D] p-2 hover:outline hover:outline-[#1A1A1A]"
 			>
 				<h1>Coal <span class="opacity-75">/ USD</span></h1>
 			</div>
 
-			<div class="h-[250px] w-[250px] rounded-full border-[1px] border-[#404040]/25"></div>
+			<div
+				class="h-[250px] w-[400px] rounded-lg border-[1px] border-[#404040]/25 bg-[#0D0D0D] p-2 hover:outline hover:outline-[#1A1A1A]"
+			>
+				<h1>USD <span class="opacity-75">/ MNT</span></h1>
+			</div>
+
+			<div
+				class="h-[250px] w-[400px] rounded-lg border-[1px] border-[#404040]/25 bg-[#0D0D0D] p-2 hover:outline hover:outline-[#1A1A1A]"
+			>
+				<h1>Net inflation</h1>
+			</div>
 		</div>
 	</div>
 </div>
